375779c6938d32a0dc73a556d588182edafaead7
Ched2370 <mmibz.85@gmail.com>
2024-06-28 16:53:13 -0300

    ejercicio 2 terminado

--- ejercicio_02/autopista.cpp
+++ ejercicio_02/autopista.cpp
@@ -2,71 +2,82 @@
 #include <queue>
 #include <cstring>
 #include <string>
-
+#include <locale.h>
+#include <conio.h>
 using namespace std;
 
-// Definición de la estructura Auto
-struct Auto {
+struct Auto
+{
     char modelo[20];
     char patente[20];
 };
-    Auto auto1 = {"vento", "AZ 234 RT"};
- 
+Auto auto1 = {"Vento", "AZ 234 RT"};
+Auto auto2 = {"Cronos", "AS 459 GT"};
 
-    Auto auto2 = {"Cronos", "AS 459 GT"};
-    
-// Dos colas para las dos vías
 queue<Auto> Via0;
 queue<Auto> Via1;
 
-// Funciones para insertar en las vías
-void insertarV0(Auto auto1) {
-    if (Via0.size() < 5) { // Verificar si hay espacio en Via0
+void insertarV0(Auto auto1)
+{
+    if (Via0.size() < 5)
+    {
         Via0.push(auto1);
-        cout << "Vehículo ingresado en Via0: " << auto1.modelo << ", " << auto1.patente << endl;
-    } else {
+        cout << "Ingreso a Via0(via comun), vehiculo: " << auto1.modelo << ", " << auto1.patente << endl;
+    }
+    else
+    {
         cout << "Via0 está llena." << endl;
     }
 }
 
-void insertarV1( Auto auto2) {
-    if (Via1.size() < 5) { // Verificar si hay espacio en Via1
+void insertarV1(Auto auto2)
+{
+    if (Via1.size() < 5)
+    {
         Via1.push(auto2);
-        cout << "Vehículo ingresado en Via1: " << auto2.modelo << ", " << auto2.patente << endl;
-    } else {
+        cout << "Ingreso a Via1(via rapida), vehiculo: " << auto2.modelo << ", " << auto2.patente << endl;
+    }
+    else
+    {
         cout << "Via1 está llena." << endl;
     }
 }
 
-// Funciones para suprimir de las vías
-void suprimirV0() {
-    if (!Via0.empty()) {
-        Via0.front();
-        cout << "Vehículo eliminado de Via0: "<< endl;
+void suprimirV0()
+{
+    if (!Via0.empty())
+    {
+        cout << "Egreso de Via0(via comun), vehiculo: " << Via0.front().modelo << ", " << Via0.front().patente << endl;
         Via0.pop();
-    } else {
+    }
+    else
+    {
         cout << "Via0 está vacía." << endl;
     }
 }
 
-void suprimirV1() {
-    if (!Via1.empty()) {
-       Via1.front();
-        cout << "Vehículo eliminado de Via1: " << endl;
+void suprimirV1()
+{
+    if (!Via1.empty())
+    {
+        cout << "Egreso de Via1(via rapida), vehiculo: " << Via1.front().modelo << ", " << Via1.front().patente << endl;
         Via1.pop();
-    } else {
+    }
+    else
+    {
         cout << "Via1 está vacía." << endl;
     }
 }
 
-int main() {
-   
+int main()
+{
 
     insertarV0(auto1);
     insertarV1(auto2);
-
-    suprimirV0();
+    cout << endl;
     suprimirV1();
+    suprimirV0();
 
+    getch();
     return 0;
 }
